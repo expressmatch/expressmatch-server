85ae9355b60cdfc875c162c51fd82d46fd134e2b
BadriNarayanan Sridharan <badri@BadriNarayanans-MacBook-Pro-2.local>
2018-07-27 23:52:05 -0700

    Added latest dev dependency versions

--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = (props) => {
-	return (
-		<div>Hello Server App</div>
-	);
-};
-
-export default App;
---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class App extends React.Component{
+	render(){
+		return (
+			<div>Hello Server App UI</div>
+		);
+	}
+}
+export default App;
