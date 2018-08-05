f631eb35e09b2992eb5de86d1b07b014fd797390
BadriNarayanan Sridharan <badri@BadriNarayanans-MacBook-Pro-2.local>
2018-08-05 08:00:45 -0700

    Adding Login page and refactored routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,4 +1,77 @@
 const express = require("express");
 const router = express.Router();
 
-module.exports = router;
+module.exports = function(app, passport){
+
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    app.get('/', function(req, res) {
+        if(req.isAuthenticated()){
+            res.redirect('/posts');
+        }else{
+            res.render('index.ejs');
+        }
+    });
+
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    app.get('/login', function(req, res) {
+        if(req.isAuthenticated()){
+            res.redirect('/posts');
+        }else{
+            res.render('emlogin.ejs', { message: req.flash('loginMessage') });
+        }
+    });
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/posts', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // =====================================
+    // SIGN UP =============================
+    // =====================================
+    app.get('/signup', function(req, res) {
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/posts', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    app.get('/auth/facebook', passport.authenticate('facebook', {
+        scope : ['public_profile', 'email']
+    }));
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/posts',
+            failureRedirect : '/'
+        }));
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    // =====================================
+    // APP AUTHENTICATION FOR ALL ROUTES ===
+    // =====================================
+    app.use('/', function(req, res){
+        if(req.isAuthenticated()){
+            req.next();
+        }else{
+            res.redirect('/');
+        }
+    });
+
+    return router;
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,74 +6,10 @@ const router = express.Router();
 
 module.exports = function(app, passport) {
 
-	// =====================================
-	// HOME PAGE (with login links) ========
-	// =====================================
-	app.get('/', function(req, res) {
-	    res.render('index.ejs'); // load the index.ejs file
-	});
+    app.use("/auth", authRoutes(app, passport));
 
-	// =====================================
-	// LOGIN ===============================
-	// =====================================
-	// show the login form
-	app.get('/login', function(req, res) {
-
-	    // render the page and pass in any flash data if it exists
-	    res.render('login.ejs', { message: req.flash('loginMessage') }); 
-	});
-
-	// process the login form
-    app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/posts', // redirect to the secure profile section
-        failureRedirect : '/login', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
-
-	// =====================================
-	// SIGNUP ==============================
-	// =====================================
-	// show the signup form
-	app.get('/signup', function(req, res) {
-
-	    // render the page and pass in any flash data if it exists
-	    res.render('signup.ejs', { message: req.flash('signupMessage') });
-	});
-
-	// process the signup form
-	app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/posts', // redirect to the secure profile section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
-
-	// =====================================
-    // FACEBOOK ROUTES =====================
-    // =====================================
-    // route for facebook authentication and login
-    app.get('/auth/facebook', passport.authenticate('facebook', { 
-      scope : ['public_profile', 'email']
-    }));
-
-    // handle the callback after facebook has authenticated the user
-    app.get('/auth/facebook/callback',
-        passport.authenticate('facebook', {
-            successRedirect : '/posts',
-            failureRedirect : '/'
-        }));
-
-    // =====================================
-    // APP AUTHENTICATION FOR ALL ROUTES ===
-    // =====================================
-    // authenticating rest of the server and UI routes
-    app.use('/', function(req, res){
-    	console.log('Authentication middleware called at /');
-    	if(req.isAuthenticated()){
-    		req.next();
-    	}else{
-    		res.redirect('/');
-    	}
-    });
+    app.use("/user", isLoggedIn, userRoutes);
+    app.use("/", isLoggedIn, postRoutes(app));
 
 	// =====================================
 	// PROFILE SECTION =====================
@@ -86,34 +22,17 @@ module.exports = function(app, passport) {
 	    });
 	});
 
-	// =====================================
-	// LOGOUT ==============================
-	// =====================================
-	app.get('/logout', function(req, res) {
-	    req.logout();
-	    res.redirect('/');
-	});
-
-	app.use("/user", userRoutes);
-	app.use("/auth", authRoutes);
-	app.use(postRoutes(app));
-
 	console.log(listRoutes(app));
 	return router;
 }
 
-
-// route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
 
-    // if user is authenticated in the session, carry on
     if (req.isAuthenticated())
         return next();
 
-    // if they aren't redirect them to the home page
     res.redirect('/');
 }
-
 
 function listRoutes(app, routes, stack, parent){
 
