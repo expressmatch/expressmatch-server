d5ef16911551a9b0057c1930f4a8c2c8435d0f85
Express Match <expresstomatch@gmail.com>
2020-05-13 10:11:34 +0530

    Theme changes for server and some fixes in client side

--- client/src/components/common/Spinner.js
+++ client/src/components/common/Spinner.js
@@ -30,7 +30,7 @@ export default class Spinner extends React.Component {
                             className={override}
                             sizeUnit={"px"}
                             size={150}
-                            color={'#545566'}
+                            color={'#fff'}
                             loading={this.state.loading}
                         />
                     </div>
