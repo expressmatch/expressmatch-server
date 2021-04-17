11e7c2613274f0baf74a29cbcab125b9d1071184
Express Match <expresstomatch@gmail.com>
2021-04-17 13:44:51 +0530

    Updating responsive side on server pages and enhancing index page.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
         return (
             <div id="footer">
                 <div className="footer-message">
-                    Copyright @ 2020 Express To Match, India. All rights reserved.&nbsp;
+                    Copyright @ 2021 Express To Match, India. All rights reserved.&nbsp;
                     <a href="/privacy" target="_blank">Privacy Policy</a> and <a href="/terms" target="_blank">Terms of Use</a>
                 </div>
             </div>
